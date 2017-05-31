3e0ab1ad818ab81aff8cfebccaaede7cb4832718
Ricardo Bravo <rbravo@datiobd.com>
2017-05-31 17:17:50 +0200

    Update scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -20,12 +20,12 @@ TASK_MEM = 32
 EXECUTOR_CPUS = 1
 EXECUTOR_MEM = 32
 
-class MinimalMesosSchedulerDriver(MesosSchedulerDriver):
-    def launchTasks(self, offerIds, tasks, filters=None):
-        logging.info("************LAUNCH TASKS ") 
-        logging.info(tasks)
-        logging.info("************LAUNCH TASKS") 
-        MesosSchedulerDriver.launchTasks(self,offerIds,tasks, filters)
+#class MinimalMesosSchedulerDriver(MesosSchedulerDriver):
+#    def launchTasks(self, offerIds, tasks, filters=None):
+#        logging.info("************LAUNCH TASKS ") 
+#        logging.info(tasks)
+#        logging.info("************LAUNCH TASKS") 
+#        MesosSchedulerDriver.launchTasks(self,offerIds,tasks, filters)
 
 
 class MinimalScheduler(Scheduler):
@@ -48,38 +48,48 @@ class MinimalScheduler(Scheduler):
         logginf.info(self)
         logginf.info(driver)
         logging.info("************RE RE gistered") 
+    def checkTask():
+        logging.info("redis-------------------------")
+        logging.info(frameworkId)
+        self._redis.decr('foo')
+        logging.info(self._redis.get('foo'))
+        #queue????
+        if self._redis.decr('foo')<0:
+            raise TaskException('maximum number of tasks')
 
     def resourceOffers(self, driver, offers):
         filters = {'refuse_seconds': 5}        
+        
         for offer in offers:
-            cpus = self.getResource(offer.resources, 'cpus')
-            mem = self.getResource(offer.resources, 'mem')
-            if cpus < TASK_CPU or mem < TASK_MEM:
-                continue
+            try:
+                cpus = self.getResource(offer.resources, 'cpus')
+                mem = self.getResource(offer.resources, 'mem')
+                if cpus < TASK_CPU or mem < TASK_MEM:
+                    continue
 
-            task = Dict()
-            task_id = str(uuid.uuid4())
-            task.task_id.value = task_id
-            task.agent_id.value = offer.agent_id.value
-            task.name = 'task {}'.format(task_id)
-            task.container.type = 'DOCKER' 
-            task.container.docker.image = os.getenv('DOCKER_TASK')
-            task.container.docker.network = 'HOST'
-            task.container.docker.force_pull_image = True
+                task = Dict()
+                task_id = str(uuid.uuid4())
+                task.task_id.value = task_id
+                task.agent_id.value = offer.agent_id.value
+                task.name = 'task {}'.format(task_id)
+                task.container.type = 'DOCKER' 
+                task.container.docker.image = os.getenv('DOCKER_TASK')
+                task.container.docker.network = 'HOST'
+                task.container.docker.force_pull_image = True
 
-            task.resources = [
-                dict(name='cpus', type='SCALAR', scalar={'value': TASK_CPU}),
-                dict(name='mem', type='SCALAR', scalar={'value': TASK_MEM}),
-            ]
-            task.command.shell = True
-            task.command.value = '/app/task.sh '+self._message
-            #task.command.arguments = [self._message]
-            logging.info("redis-------------------------")
-            self._redis.decr('foo')
-            logging.info(self._redis.get('foo'))
+                task.resources = [
+                    dict(name='cpus', type='SCALAR', scalar={'value': TASK_CPU}),
+                    dict(name='mem', type='SCALAR', scalar={'value': TASK_MEM}),
+                ]
+                task.command.shell = True
+                task.command.value = '/app/task.sh '+self._message
+                #task.command.arguments = [self._message]
 
-            logging.info(task)            
-            driver.launchTasks(offer.id, [task], filters)
+                logging.info(task)            
+                driver.launchTasks(offer.id, [task], filters)
+            except TaskException:
+                logging.info("TASK EXCEPTION")
+                pass 
 
     def getResource(self, res, name):
         for r in res:
@@ -100,19 +110,19 @@ def main(message):
     framework.name = "MinimalFramework"
     framework.hostname = socket.gethostname()
 
-   # driver = MesosSchedulerDriver(
-   #     MinimalScheduler(message),
-    #    framework,
-     #          os.getenv('MASTER'),
-      #  use_addict=True,
-    #)
-    
-    driver = MinimalMesosSchedulerDriver(
+    driver = MesosSchedulerDriver(
         MinimalScheduler(message),
         framework,
                os.getenv('MASTER'),
         use_addict=True,
     )
+    
+#    driver = MinimalMesosSchedulerDriver(
+#        MinimalScheduler(message),
+#        framework,
+#               os.getenv('MASTER'),
+#        use_addict=True,
+#    )
 
     def signal_handler(signal, frame):
         driver.stop()
