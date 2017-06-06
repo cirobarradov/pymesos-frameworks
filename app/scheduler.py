07e7ca2ba02c8e6ee8385dca574ef3f9a1ff2e12
Ricardo Bravo <rbravo@datiobd.com>
2017-06-06 13:49:37 +0200

    Update scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python2.7
 from __future__ import print_function
-
 import sys
 import uuid
 import time
@@ -9,57 +7,77 @@ import signal
 import getpass
 from threading import Thread
 import os
-import redis
+import hiredis
+import json
 
 from pymesos import MesosSchedulerDriver, Scheduler, encode_data
 from addict import Dict
 
-TASK_CPU = 1
+TASK_CPU = 0.2
 TASK_MEM = 32
 EXECUTOR_CPUS = 1
 EXECUTOR_MEM = 32
 
 
 class MinimalScheduler(Scheduler):
-    def __init__(self, message):
-        self._redis = redis.StrictRedis(host=os.getenv('REDIS_SERVER'), port=6379, db=0)
+    def __init__(self, message, conn):
+        self._redis = conn
         self._message = message
 
+    '''
+    Method that checks if redis has scheduler registered 
+    '''
+    def saveOrUpdateState(self, key, max_tasks, id):
+        if self._redis.exists(key):
+            logging.info("framework already registered in redis")
+            #self._redis.hset(key, 'max_tasks', max_tasks)
+            logging.info("****************** OLD ID" + self._redis.hget(key, 'fwk_id'))
+            self._redis.hset(key, 'fwk_id', id)
+
+        else:
+            logging.info("framework NOT registered in redis")
+            self._redis.hset(key, 'max_tasks', max_tasks)
+            self._redis.hset(key, 'fwk_id', id)
+
+        logging.info("****************** NEW ID" + self._redis.hget(key, 'fwk_id'))
+        logging.info("****************** TASKS:" + self._redis.hget(key, 'max_tasks'))
+
     def registered(self, driver, frameworkId, masterInfo):
-        #set max tasks to framework registered
+        # set max tasks to framework registered
         logging.info("************registered     " + frameworkId['value'])
-        self._redis.set(frameworkId['value'], int(os.getenv('MAX_TASKS')))
-        #logging.info(masterInfo)
-        #logging.info(driver)
+        self.saveOrUpdateState(driver._framework['name'],
+                               int(os.getenv('MAX_TASKS')),
+                               frameworkId['value'])
         logging.info("<---")
 
     def reregistered(self, driver, masterInfo):
-        logging.info("************RE RE gistered")
+        logging.info("************re-registered  ")
         logging.info(masterInfo)
-        #logging.info(self)
+        # logging.info(self)
         logging.info(driver)
         logging.info("<---")
 
-    def checkTask(self, frameworkID):
+    def checkTask(self, frameworkName):
 
-        if int(self._redis.get(frameworkID)) <= 0:
-            logging.info("maximum number of tasks")
+        if int(self._redis.hget(frameworkName, 'max_tasks')) <= 0:
+            logging.info("Reached xmaximum number of tasks")
             raise Exception('maximum number of tasks')
         else:
-            logging.info("number tasks available = "+self._redis.get(frameworkID) + " of " + os.getenv("MAX_TASKS") )
-            self._redis.decr(frameworkID)
-        #logging.info(framework)
-        #logging.info(_redis.get('foo'))
-        # queue????
-        #self._redis.decr('foo')
-        #if self._redis.get('foo') < 0:
-         #   raise Exception('maximum number of tasks')
+            logging.info("number tasks available = " + self._redis.hget(frameworkName,
+                                                                        'max_tasks') + " of " + os.getenv("MAX_TASKS"))
+            self._redis.hincrby(frameworkName, 'max_tasks', -1)
+            # logging.info(framework)
+            # logging.info(_redis.get('foo'))
+            # queue????
+            # self._redis.decr('foo')
+            # if self._redis.get('foo') < 0:
+            #   raise Exception('maximum number of tasks')
 
     def resourceOffers(self, driver, offers):
         filters = {'refuse_seconds': 5}
         for offer in offers:
             try:
-                self.checkTask(driver.framework_id)
+                self.checkTask(driver._framework['name'])
                 cpus = self.getResource(offer.resources, 'cpus')
                 mem = self.getResource(offer.resources, 'mem')
                 if cpus < TASK_CPU or mem < TASK_MEM:
@@ -82,11 +100,12 @@ class MinimalScheduler(Scheduler):
                 task.command.shell = True
                 task.command.value = '/app/task.sh ' + self._message
                 # task.command.arguments = [self._message]
-                #logging.info(task)
-                logging.info("launch task name:" + task.name + " resources: "+ ",".join(str(x) for x in task.resources))
+                # logging.info(task)
+                logging.info(
+                    "launch task name:" + task.name + " resources: " + ",".join(str(x) for x in task.resources))
                 driver.launchTasks(offer.id, [task], filters)
             except Exception:
-                #traceback.print_exc()
+                # traceback.print_exc()
                 pass
 
     def getResource(self, res, name):
@@ -100,28 +119,28 @@ class MinimalScheduler(Scheduler):
                       update.task_id.value,
                       update.state)
         if update.state == "TASK_FINISHED":
-            logging.info("take another task for framework" + driver.framework_id)
-            self._redis.incr(driver.framework_id)
-            logging.info("tasks availables = " + self._redis.get(driver.framework_id)+ " of "+os.getenv("MAX_TASKS"))
-            
-    
+            logging.info("take another task for framework" + driver.framework_id + " " + driver._framework['name'])
+            self._redis.hincrby(driver._framework['name'], 'max_tasks', 1)
+            logging.info("tasks availables = " + self._redis.hget(driver._framework['name'],
+                                                                  'max_tasks') + " of " + os.getenv("MAX_TASKS"))
+
+
 def main(message):
+    connection = redis.StrictRedis(host=os.getenv('REDIS_SERVER'), port=6379, db=0)
     framework = Dict()
     framework.user = getpass.getuser()
     framework.name = "MinimalFramework"
     framework.hostname = socket.gethostname()
-    framework._id='003fba48-218a-484d-97d2-3ab5c89c9257-0277'
-    framework.id=u'003fba48-218a-484d-97d2-3ab5c89c9257-0277'
-    
+
     driver = MesosSchedulerDriver(
-        MinimalScheduler(message),
+        MinimalScheduler(message, connection),
         framework,
         os.getenv('MASTER'),
         use_addict=True,
     )
 
-
     def signal_handler(signal, frame):
+        logging.info("Stopping Driver and closing redis connection")
         driver.stop()
 
     def run_driver_thread():
